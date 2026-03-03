c1ef7ad93ae3c42e86b551dd8db2fa232721aff2
OKevey <usarparaverify@gmail.com>
2026-03-03 17:41:50 -0300

    modified:   arts.js 	renamed:    arts/DJ K3YVE PFP for spotify2.png -> arts/DJ K3YVE PFP for spotify.png 	new file:   arts/MONTAGEM LADRAO.png 	new file:   arts/MONTAGEM SUPERSONIC.png 	new file:   arts/MONTAGEM XONADA.png 	modified:   style.css

--- arts.js
+++ arts.js
@@ -39,14 +39,32 @@ const ARTS = [
   // },
 
 
-  // {
-  //    id:          'exemplo-01',
-  //    file:        'arts/DJ K3YVE PFP for spotify.png',
-  //    name:        'Exemplo 01',
-  //    description: 'Descrição da arte exemplo.',
-  //    category:    'Artes 2025',
-  //    featured:    1
-   //},
+   {
+      id:          '1',
+      file:        'arts/MONTAGEM, XONADA.png',
+      name:        'MONTAGEM XONADA',
+      description: 'Art made for DJ Javi26, MXZI, Dj Samir | Lunar media',
+      category:    '2025 ARTS',
+      featured:    1
+   },
+
+  {
+      id:          '2',
+      file:        'arts/MONTAGEM, LADRAO.png',
+      name:        'MONTAGEM LADRÃO',
+      description: 'Art made For ATLXS, MXZI | Rubix Records',
+      category:    '2025 ARTS',
+      featured:    2
+   },
+
+  {
+      id:          '3',
+      file:        'arts/MONTAGEM, SUPERSONIC.png',
+      name:        'MONTAGEM SUPERSONIC',
+      description: 'Art made For KHAOS, Jmilton, Mc jajá | Rubix Records',
+      category:    '2025 ARTS',
+      featured:    3
+   },
 
 ];
 
@@ -57,5 +75,5 @@ const ARTS = [
 const CONFIG = {
   name:     'K3Y.',                  // Nome/logo no header e footer
   tagline:  'All rights Reserved.',
-  about:    '',
+  about:    'DJ K3YVE is a Brazilian DJ and music producer known for blending funk with modern electronic influences. His sound explores energetic rhythms, heavy basslines and catchy melodies, creating tracks that feel raw. \n\nK3YVE is also a Cover-art designer, working specially for Rubix Records™ and Selected Artists, such as "DJ Javi26".\n\nHis arts travels around all the world, Being a Inspiration to people and going through worldwide hits.',
 };
